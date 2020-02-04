7168e233053047c15cc03fd3b29afe46ef6c4790
Jessica Albert <jessmalbert0807@gmail.com>
2020-02-03 18:58:42 -0600

    Adding Node JS skeleton
    Copied hello-handlebars project files from CS290. Updated views and js file with our page names

---
+++ source-files/helloHandlebars.js
@@ -0,0 +1,40 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+  res.render('home');
+});
+
+app.get('/browse-recipes',function(req,res){
+  res.render('browse-recipes');
+});
+
+app.get('/customer-info',function(req,res){
+  res.render('customer-info');
+});
+
+app.get('/login',function(req,res){
+  res.render('login');
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
