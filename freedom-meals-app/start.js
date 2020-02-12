8d970f24ed7f61a026e95ee3269f418611a72561
Jessica Albert <jessmalbert0807@gmail.com>
2020-02-12 17:21:47 -0600

    See last commit for start.js

--- freedom-meals-app/start.js
+++ freedom-meals-app/start.js
@@ -1,6 +1,7 @@
 //Import app created in app.js
 const app = require('./app');
 
+//App listening on port 3000
 const server = app.listen(3000, () =>{
     console.log('Express is running on port ${server.address().port}');
 });
