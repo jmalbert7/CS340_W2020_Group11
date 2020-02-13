9f3b22c148370d69b4cfb56c695700c4e830bad0
Jessica Albert <jessmalbert0807@gmail.com>
2020-02-12 19:46:16 -0600

    Handlebars set up and home page migrated
    Added express-handlebars module and added appropriate imports to app.js. Created views folder and migrated index.js from PS3 to home.handlebars. Also moved images folder into public directory.

--- freedom-meals-app/app.js
+++ freedom-meals-app/app.js
@@ -1,12 +1,22 @@
 //import modules
 const express = require('express');
+const path = require('path');   //for working with file directory
 const routes = require('./routes/index');
+
 
 //Create the express application
 const app = express();
 
+//Refer to handlebars documentation for this, CS290 info seems to be out of date
+const handlebars = require('express-handlebars');
+
+app.engine('handlebars', handlebars());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
 //When any request to app is received, use the routes file
 app.use('/', routes);
+app.use(express.static('public'));
 
 //Export app so it can be used in other files
 module.exports = app;
--- freedom-meals-app/routes/index.js
+++ freedom-meals-app/routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-    res.send('It works!');
+    res.render('home', {title:'Freedom Meals - Home'});
 });
 
 module.exports = router;
