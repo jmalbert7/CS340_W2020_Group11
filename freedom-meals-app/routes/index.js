101478132f3fb9bc4715fe3da855acbfce912fb0
Jessica Albert <jessmalbert0807@gmail.com>
2020-02-12 19:56:38 -0600

    Linked recipes page
    Linked recipes page in nav bar.  SO far only rendering static information.

--- freedom-meals-app/routes/index.js
+++ freedom-meals-app/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) =>{
     res.render('home', {title:'Freedom Meals - Home'});
 });
 
+router.get('/recipes', (req, res) =>{
+    res.render('recipes', {title:'Browse Recipes'});
+});
+
 module.exports = router;
