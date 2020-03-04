808e5fff438485622395ce9f311fdca4abb5a533
Jessica Albert <jessmalbert0807@gmail.com>
2020-03-04 10:30:23 -0600

    Remove recipe is functional
    Not technically working ajax style, but this is OK for now. Javascript function reloads the /orders page once the cart is updated.

--- freedom-meals-app/public/js/orders.js
+++ freedom-meals-app/public/js/orders.js
@@ -1,1 +1,21 @@
 /* Insert code here for orders page. */
+
+function removeRecipeFromCart(recipe_id){
+    var req = new XMLHttpRequest();
+    console.log('REMOVE');
+	console.log("sent data" + recipe_id);
+	var sdata = {"hiddenRecipeId" : recipe_id, "method":"remove"};
+	console.log("sdata" + sdata);
+	console.log(JSON.stringify(sdata));
+	req.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+            console.log('success?');
+            location.reload();
+        }
+	};
+	req.open('POST', '/orders/remove', true);
+	req.setRequestHeader('Content-Type', 'application/json');
+	
+	req.send(JSON.stringify(sdata));
+	event.preventDefault();
+}
