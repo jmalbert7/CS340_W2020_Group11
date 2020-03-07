adc5997dbc91bc598d22329eb45a4a2728d284d0
Jessica Albert <jessmalbert0807@gmail.com>
2020-03-07 09:57:12 -0600

    check if logged in IP

--- freedom-meals-app/routes/index.js
+++ freedom-meals-app/routes/index.js
@@ -9,7 +9,14 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
-app.use(session({secret:'SuperSecretPassword'}));
+app.use(session({ secret: 'SuperSecretPassword' }));
+
+function checkIfLoggedIn(loggedin) {
+    if (loggedin === false || loggedin == null) {
+        return false;
+    }
+    return true;
+}
 
 //router responds to any requests from the root url
 var homepage = express.Router();
@@ -26,7 +33,7 @@ homepage.get('/', (req, res) => {
         }
     ];*/
     console.log('in the homepage / router');
-    if(!req.session.cart){
+    if (!req.session.cart) {
         console.log('new session');
         req.session.cart = [];
     }
@@ -60,12 +67,12 @@ homepage.get('/:time', (req, res) => {
 //in the session array that holds the user's recipe selections
 homepage.post('/', (req, res) => {
     console.log("request body " + req.body.hiddenRecipeId);
-    
+
     req.session.cart.push(req.body.hiddenRecipeId);
     console.log('length ' + req.session.cart.length);
-    req.session.cart.forEach(element => { 
-        console.log("loop " + element); 
-    }); 
+    req.session.cart.forEach(element => {
+        console.log("loop " + element);
+    });
     req.session.save();
     res.status = 200;
     res.readystate = 4;
@@ -74,22 +81,20 @@ homepage.post('/', (req, res) => {
 //homepage router to add a new recipe to the recipes table
 homepage.post('/add', (req, res) => {
     console.log('inside add recipe route, customer_id = ' + req.session.customer_id);
-	console.log(req.body)
-	var mysql = req.app.get('mysql');
-	var sql = "INSERT INTO Recipes (recipe_name, time, difficulty, directions) VALUES (?,?,?,?)";
-	var inserts = [req.body.recipeName, req.body.time, req.body.difficulty, req.body.directions];
-	sql = mysql.pool.query(sql, inserts, function(error, results, fields){
-		if(error)
-		{
-			console.log(JSON.stringify(error))
-			res.write(JSON.stringify(error));
-			res.end();
-		}
-		else
-		{
-			res.redirect('/recipes');
-		}
-	});
+    console.log(req.body)
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO Recipes (recipe_name, time, difficulty, directions) VALUES (?,?,?,?)";
+    var inserts = [req.body.recipeName, req.body.time, req.body.difficulty, req.body.directions];
+    sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+        if (error) {
+            console.log(JSON.stringify(error))
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+        else {
+            res.redirect('/recipes');
+        }
+    });
 });
 app.use('/recipes', homepage);
 
@@ -98,32 +103,38 @@ var orderpage = express.Router();
 
 //Displays order history for customer, customer must be logged in first
 orderpage.get('/', (req, res) => {
+    var loggedin = req.session.loggedin;
+    /*if (!checkIfLoggedIn(loggedin)) {
+        req.session.lastLink = '/orders';
+        req.session.save();
+        res.redirect('/login');
+    }*/
     var orders = [];
     var cart = [];
-    console.log("req body in get request " + req.body.method);
-    
-    req.session.cart.forEach(element => { 
-        cart.push({recipe_id : element, recipe_name: null});
+    console.log("req body in orderpage get request " + req.body.method);
+
+    req.session.cart.forEach(element => {
+        cart.push({ recipe_id: element, recipe_name: null });
     });
 
-    cart.forEach(element => { 
+    cart.forEach(element => {
         console.log("cart " + element.recipe_id);
     });
 
-    cart.forEach(element => {       
+    cart.forEach(element => {
         var sql2 = "SELECT recipe_name FROM Recipes WHERE recipe_id = ?";
         mysql.pool.query(sql2, [element.recipe_id], function (err, recipe) {
-        if (err) {
-            next(err);
-            return;
-        }
-        console.log(recipe[0].recipe_name);
-        console.log(element.recipe_name);
-        element.recipe_name = recipe[0].recipe_name;
-        console.log(element.recipe_name);
+            if (err) {
+                next(err);
+                return;
+            }
+            console.log(recipe[0].recipe_name);
+            console.log(element.recipe_name);
+            element.recipe_name = recipe[0].recipe_name;
+            console.log(element.recipe_name);
         });
     });
-       
+
     console.log(req.session.customer_id);
     var customer_id = req.session.customer_id;
     var sql = "SELECT Orders.order_id, DATE_FORMAT(Orders.order_date, \'%m/%d/%Y\') AS order_date, DATE_FORMAT(Orders.delivery_date, \'%m/%d/%Y\') AS delivery_date, Orders.order_status, Recipes.recipe_name FROM Orders JOIN Recipes_in_Orders ON Orders.order_id = Recipes_in_Orders.order_id JOIN Recipes ON Recipes_in_Orders.recipe_id = Recipes.recipe_id WHERE customer_id = ? ORDER BY order_date DESC;";
@@ -134,27 +145,27 @@ orderpage.get('/', (req, res) => {
         }
         let orders = rows;
         console.log(orders);
-        
-        res.render('orders', { title: 'Your Orders', orders, cart});
+
+        res.render('orders', { title: 'Your Orders', orders, cart });
     });
 });
 
 //This route removes a recipe from the cart session array when a user 
-orderpage.post('/remove', (req, res) => { 
+orderpage.post('/remove', (req, res) => {
     console.log("REMOVE: request body " + req.body.hiddenRecipeId);
     var value = req.body.hiddenRecipeId;
-    
-    for(var i = 0; i < req.session.cart.length; i++){
+
+    for (var i = 0; i < req.session.cart.length; i++) {
         console.log("in remove loop " + req.session.cart[i])
-        if(req.session.cart[i] === value){
+        if (req.session.cart[i] === value) {
             req.session.cart.splice(i, 1);
             console.log("Recipe removed: " + req.body.hiddenRecipeId);
         }
     }
     console.log('length ' + req.session.cart.length);
-    req.session.cart.forEach(element => { 
+    req.session.cart.forEach(element => {
         console.log("loop " + element);
-    }); 
+    });
     req.session.save();
     res.status = 200;
     res.readystate = 4;
@@ -162,7 +173,7 @@ orderpage.post('/remove', (req, res) => {
 });
 
 //This route places the order for the recipes currently in the cart
-orderpage.post('/add/:id', (req, res) =>{
+orderpage.post('/add/:id', (req, res) => {
 
 });
 app.use('/orders', orderpage);
@@ -171,66 +182,58 @@ app.use('/orders', orderpage);
 var ratingpage = express.Router();
 
 //Display user's ratings for all recipes they have ordered
-ratingpage.get('/', (req, res) =>{
+ratingpage.get('/', (req, res) => {
     var recipeRatings = [];
     console.log('in rating get');
     console.log("customer_id = " + req.session.customer_id);
     var customer_id = req.session.customer_id;
-	var sql = "SELECT Recipes.recipe_id, Recipes.recipe_name, Recipe_Ratings.rating, DATE_FORMAT(Recipe_Ratings.date_rated, \'%m/%d/%Y\') AS date_rated FROM Orders LEFT JOIN ((Recipes_in_Orders LEFT JOIN Recipes ON Recipes_in_Orders.recipe_id = Recipes.recipe_id) LEFT JOIN Recipe_Ratings ON Recipes_in_Orders.recipe_id = Recipe_Ratings.recipe_id) ON Orders.order_id = Recipes_in_Orders.order_id WHERE (Recipe_Ratings.customer_id = ? AND Orders.customer_id = ?) OR (Recipe_Ratings.customer_id IS NULL AND Orders.customer_id = ?)";
-    mysql.pool.query(sql, [customer_id, customer_id, customer_id], function(err, rows, fields){
+    var sql = "SELECT Recipes.recipe_id, Recipes.recipe_name, Recipe_Ratings.rating, DATE_FORMAT(Recipe_Ratings.date_rated, \'%m/%d/%Y\') AS date_rated FROM Orders LEFT JOIN ((Recipes_in_Orders LEFT JOIN Recipes ON Recipes_in_Orders.recipe_id = Recipes.recipe_id) LEFT JOIN Recipe_Ratings ON Recipes_in_Orders.recipe_id = Recipe_Ratings.recipe_id) ON Orders.order_id = Recipes_in_Orders.order_id WHERE (Recipe_Ratings.customer_id = ? AND Orders.customer_id = ?) OR (Recipe_Ratings.customer_id IS NULL AND Orders.customer_id = ?)";
+    mysql.pool.query(sql, [customer_id, customer_id, customer_id], function (err, rows, fields) {
         if (err) {
             console.log(JSON.stringify(err));
-			res.end();
+            res.end();
         }
         let recipeRatings = rows;
         console.log(recipeRatings);
-        res.render('rating', { title: 'Your Ratings', recipeRatings});
+        res.render('rating', { title: 'Your Ratings', recipeRatings });
     });
 });
 
 //Rating router to add or update rating
 ratingpage.post('/add', (req, res) => {
     console.log('inside add ratings route, customer_id = ' + req.session.customer_id);
-	console.log(req.body);
-	var mysql = req.app.get('mysql');
-	var customer_id = req.session.customer_id;
-	var ratingType = req.body.ratingType;
-	
-	// If user is adding a new rating...
-	if (ratingType == "NEW")
-	{
-		var sql = "INSERT INTO Recipe_Ratings (`rating`, `date_rated`, `customer_id`, `recipe_id`) VALUES (?, (SYSDATE()), ?, ?)";
-		mysql.pool.query(sql, [req.body.rating, customer_id, req.body.recipeID], function(error, results, fields)
-		{
-			if(error)
-			{
-				console.log(JSON.stringify(error));
-				res.end();
-			}
-			else
-			{
-				res.redirect('/rating');
-			}
-		});
-	}
-	
-	// If user is updating an existing rating...
-	if (ratingType == "EXISTING")
-	{
-		var sql = "UPDATE Recipe_Ratings SET rating = ?, date_rated = (SYSDATE()) WHERE customer_id = ? AND recipe_id = ?";
-		mysql.pool.query(sql, [req.body.rating, customer_id, req.body.recipeID], function(error, results, fields)
-		{
-			if(error)
-			{
-				console.log(JSON.stringify(error));
-				res.end();
-			}
-			else
-			{
-				res.redirect('/rating');
-			}
-		});
-	}
+    console.log(req.body);
+    var mysql = req.app.get('mysql');
+    var customer_id = req.session.customer_id;
+    var ratingType = req.body.ratingType;
+
+    // If user is adding a new rating...
+    if (ratingType == "NEW") {
+        var sql = "INSERT INTO Recipe_Ratings (`rating`, `date_rated`, `customer_id`, `recipe_id`) VALUES (?, (SYSDATE()), ?, ?)";
+        mysql.pool.query(sql, [req.body.rating, customer_id, req.body.recipeID], function (error, results, fields) {
+            if (error) {
+                console.log(JSON.stringify(error));
+                res.end();
+            }
+            else {
+                res.redirect('/rating');
+            }
+        });
+    }
+
+    // If user is updating an existing rating...
+    if (ratingType == "EXISTING") {
+        var sql = "UPDATE Recipe_Ratings SET rating = ?, date_rated = (SYSDATE()) WHERE customer_id = ? AND recipe_id = ?";
+        mysql.pool.query(sql, [req.body.rating, customer_id, req.body.recipeID], function (error, results, fields) {
+            if (error) {
+                console.log(JSON.stringify(error));
+                res.end();
+            }
+            else {
+                res.redirect('/rating');
+            }
+        });
+    }
 });
 app.use('/rating', ratingpage);
 
@@ -238,21 +241,25 @@ app.use('/rating', ratingpage);
 var profilepage = express.Router();
 
 //Display User's Customer information
-profilepage.get('/', (req, res) =>{
+profilepage.get('/', (req, res) => {
     var customerInfo = [];
     console.log('in profile get');
     console.log(req.session.customer_id);
     var customer_id = req.session.customer_id;
-    var sql = "SELECT first_name, last_name, email, password, phone, admin FROM Customers WHERE customer_id = ?";
-    mysql.pool.query(sql, [customer_id], function(err, rows, fields){
+    var sql = "SELECT customer_id, first_name, last_name, email, password, phone, admin FROM Customers WHERE customer_id = ?";
+    mysql.pool.query(sql, [customer_id], function (err, rows, fields) {
         if (err) {
             next(err);
             return;
         }
         let customerInfo = rows;
         console.log(customerInfo);
-        res.render('profile', { title: 'Your Profile', customerInfo});
+        res.render('profile', { title: 'Your Profile', customerInfo });
     });
+});
+
+profilepage.post('/update', (req, res) => {
+
 });
 app.use('/profile', profilepage);
 
@@ -260,9 +267,9 @@ app.use('/profile', profilepage);
 var loginpage = express.Router();
 
 //Simply displays the login page
-loginpage.get('/', (req, res) =>{
+loginpage.get('/', (req, res) => {
     console.log('in the login router');
-    res.render('login', { title: 'Login'})
+    res.render('login', { title: 'Login' })
 });
 
 //Route to sign in an existing user
@@ -271,17 +278,22 @@ loginpage.post('/auth', (req, res) => {
     var userEmail = req.body.email;
     var password = req.body.password;
     var sql = "SELECT `customer_id` FROM `Customers` WHERE `email` = ? AND `password` = ?";
-    if(userEmail && password){
-        mysql.pool.query(sql, [userEmail, password], function(err, rows, fields){
-            if(rows.length > 0){
+    if (userEmail && password) {
+        mysql.pool.query(sql, [userEmail, password], function (err, rows, fields) {
+            if (rows.length > 0) {
                 console.log(rows);
                 const context = rows;
                 console.log(context[0]);
                 req.session.loggedin = true;
                 req.session.customer_id = context[0].customer_id;
+                req.session.save();
                 console.log(req.session.customer_id);
-                res.redirect('/recipes');
-            } else{
+                if (req.session.lastLink != null) {
+                    res.redirect(req.session.lastLink);
+                } else {
+                    res.redirect('/recipes');
+                }
+            } else {
                 res.send('Incorrect username and or password');
             }
             res.end();
@@ -293,7 +305,7 @@ loginpage.post('/auth', (req, res) => {
 });
 
 //Route to create a new user
-loginpage.post('/', (req, res) =>{
+loginpage.post('/', (req, res) => {
     console.log(req.body);
     var first_name = req.body.inputFirstName;
     var last_name = req.body.inputLastName;
@@ -303,16 +315,18 @@ loginpage.post('/', (req, res) =>{
 
     var sql = "INSERT INTO `Customers` (`first_name`, `last_name`, `email`, `password`, `phone`, `admin`) VALUES (?, ?, ?, ?, ?, '1')";
 
-    if(first_name && last_name && email && phone && password){
-        mysql.pool.query(sql, [first_name, last_name, email, password, phone], function(err, rows, fields){
+    if (first_name && last_name && email && phone && password) {
+        mysql.pool.query(sql, [first_name, last_name, email, password, phone], function (err, rows, fields) {
             console.log('customer added');
             sql = "SELECT `customer_id` FROM `Customers` WHERE `email` = ? AND `password` = ?";
-            mysql.pool.query(sql, [email, password], function(err, rows, fields){
+            mysql.pool.query(sql, [email, password], function (err, rows, fields) {
                 console.log(rows);
                 var context = rows;
                 console.log(context[0]);
                 console.log(context[0].customer_id)
+                req.session.loggedin = true;
                 req.session.customer_id = context[0].customer_id;
+                req.session.save();
                 res.redirect('/recipes');
             });
         });
